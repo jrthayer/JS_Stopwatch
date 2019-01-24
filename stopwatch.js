a1657b3cb2a65a780da86b7ab052a41272e8fd29
James Thayer <jrthayer@users.noreply.github.com>
2019-01-23 17:30:27 -0800

    Add Lap Functionality
    *records stopwatch time each press of the lap button without interrupting stopwatch

--- stopwatch.js
+++ stopwatch.js
@@ -1,12 +1,15 @@
 var swDisplay = document.getElementById("swDisplay");
 var swStartStopBtn = document.getElementById("swStartStop");
 var swResetBtn = document.getElementById("swReset");
+var swLapBtn   = document.getElementById("swLapBtn");
+var swLaps = document.getElementById("swLaps");
 
 var swAmount = 0;
 var swTimes = [0,0,0,0];
 var swMaxs = [1000, 60, 60, 24];
 var swTimeout = null;
 var swRunning = false;
+var swDisplayTxt = "00:00:00:000";
 
 //used when to start or continue sw
 function swStart(){
@@ -15,34 +18,49 @@ function swStart(){
     swRun(curTime);
 }
 
+//used to stop sw
+function swStop(){
+    swRunning = false;
+}
+
+function swStartOrStop(){
+    if(swRunning){
+        swStop();
+        swStartStopBtn.textContent = "Start";
+    }
+    else{
+        swStart();
+        swStartStopBtn.textContent = "Stop";
+    }
+}
+
+//used to reset sw
+function swReset(){
+    swStop();
+    swStartStopBtn.textContent = "Start";
+    swAmount = 0;
+    swTimes = [0,0,0,0];
+    swDisplayTxt = "00:00:00:000";
+    swDisplay.textContent = swDisplayTxt;
+    swClearChildren(swLaps);
+}
+
 //sets and increments stopwatch value
 function swRun(prevTime){
     if(swRunning){
         var curTime = Date.now();
         swAmount += curTime - prevTime;
         swConvertFromMS(swAmount);
-        swDisplay.textContent = swFormatTime();
+        swDisplayTxt = swFormatTime();
+        swDisplay.textContent = swDisplayTxt;
         swTimeout = setTimeout(function(){swRun(curTime);}, 100);
     }
-    else
-    {
+    else{
         clearTimeout(swTimeout);
     }
 }
 
-//used to stop sw
-function swStop(){
-    swRunning = false;
-}
-
-//used to reset sw
-function swReset(){
-    swStop();
-    swAmount = 0;
-    swConvertFromMS(swAmount);
-    swDisplay.textContent = swFormatTime();
-}
-
+//conversion and formatting functions
 //convert ms value into hr/min/sec/ms
 function swConvertFromMS(msTime){
     // swTimes[0] = msTime%100;
@@ -74,23 +92,27 @@ function swFormatTime(){
     else if(swTimes[0]<100){
         display = display + "0" +swTimes[0];
     }
-    else
-    {
+    else{
         display = display + swTimes[0];
     }
     return display;
 }
 
-function swStartOrStop(){
-    if(swRunning){
-        swStop();
-        swStartStopBtn.textContent = "Start";
-    }
-    else{
-        swStart();
-        swStartStopBtn.textContent = "Stop";
+//Lap Functions
+//Adds laps
+function swLap(){
+    var lap = document.createElement("li");
+    lap.textContent = swDisplayTxt;
+    swLaps.appendChild(lap);  
+}
+
+//Removes All Laps
+function swClearChildren(element){
+    while(element.lastChild){
+        element.removeChild(element.lastChild);
     }
 }
 
 swStartStopBtn.addEventListener("click", swStartOrStop);
 swResetBtn.addEventListener("click", swReset);
+swLapBtn.addEventListener("click", swLap);
