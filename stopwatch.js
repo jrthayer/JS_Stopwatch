5529197be21a5f39a6d2f06b335d26138205a15e
James Thayer <jrthayer@users.noreply.github.com>
2019-01-30 21:32:17 -0800

    Convert stopwatch js into class

--- stopwatch.js
+++ stopwatch.js
@@ -1,200 +1,208 @@
-var swDisplay = document.getElementById("swDisplay");
-var swStartStopBtn = document.getElementById("swStartStop");
-var swResetBtn = document.getElementById("swReset");
-var swLapBtn   = document.getElementById("swLapBtn");
-var swLaps = document.getElementById("swLaps");
-var swLapsUpBtn = document.getElementById("swLapsUp");
-var swLapsDownBtn = document.getElementById("swLapsDown");
-var swLapsPanel = document.getElementById("swLapsPanel");
+class Stopwatch{
+    constructor(stopwatch){
+        this.swDisplay = document.getElementById("swDisplay");
+        this.swStartStopBtn = document.getElementById("swStartStop");
+        this.swResetBtn = document.getElementById("swReset");
+        this.swLapBtn   = document.getElementById("swLapBtn");
+        this.swLaps = document.getElementById("swLaps");
+        this.swLapsUpBtn = document.getElementById("swLapsUp");
+        this.swLapsDownBtn = document.getElementById("swLapsDown");
+        this.swLapsPanel = document.getElementById("swLapsPanel");
 
-var swAmount = 0;
-var swTimes = [0,0,0,0];
-var swMaxs = [1000, 60, 60, 24];
-var swTimeout = null;
-var swRunning = false;
-var swDisplayTxt = "00:00:00:000";
-var lapPage = 0;
-var lapPerPage = 3;
+        this.swAmount = 0;
+        this.swTimes = [0,0,0,0];
+        this.swMaxs = [1000, 60, 60, 24];
+        this.swTimeout = null;
+        this.swRunning = false;
+        this.swDisplayTxt = "00:00:00:000";
+        this.lapPage = 0;
+        this.lapPerPage = 3;
 
-//used when to start or continue sw
-function swStart(){
-    swRunning = true;
-    var curTime = Date.now();
-    swRun(curTime);
-}
-
-//used to stop sw
-function swStop(){
-    swRunning = false;
-}
-
-function swStartOrStop(){
-    if(swRunning){
-        swStop();
-        swStartStopBtn.textContent = "Start";
+        this.swStartStopBtn.addEventListener("click", () => this.swStartOrStop());
+        this.swResetBtn.addEventListener("click", () => this.swReset());
+        this.swLapBtn.addEventListener("click", () => this.swLap());
+        this.swLapsDownBtn.addEventListener("click", () => this.swDownPg());
+        this.swLapsUpBtn.addEventListener("click", () => this.swUpPg());
     }
-    else{
-        swStart();
-        swStartStopBtn.textContent = "Stop";
-    }
-}
 
-//used to reset sw
-function swReset(){
-    //reset stopwatch
-    swStop();
-    swStartStopBtn.textContent = "Start";
-    swAmount = 0;
-    swTimes = [0,0,0,0];
-    swDisplayTxt = "00:00:00:000";
-    swDisplay.textContent = swDisplayTxt;
-    //reset laps
-    swClearChildren(swLaps);
-    swLapsUpBtn.classList.remove("show");
-    swLapsUpBtn.classList.add("hide");
-    swLapsDownBtn.classList.remove("show");
-    swLapsDownBtn.classList.add("hide");
-    lapPage = 0;
-    swLapsPanel.classList.remove("add");
-    swLapsPanel.classList.add("remove");
-}
-
-//sets and increments stopwatch value
-function swRun(prevTime){
-    if(swRunning){
+    //used when to start or continue sw
+    swStart(){
+        this.swRunning = true;
         var curTime = Date.now();
-        swAmount += curTime - prevTime;
-        swConvertFromMS(swAmount);
-        swDisplayTxt = swFormatTime();
-        swDisplay.textContent = swDisplayTxt;
-        swTimeout = setTimeout(function(){swRun(curTime);}, 100);
-    }
-    else{
-        clearTimeout(swTimeout);
-    }
-}
-
-//conversion and formatting functions
-//convert ms value into hr/min/sec/ms
-function swConvertFromMS(msTime){
-    // swTimes[0] = msTime%100;
-    // swTimes[1] = msTime/100%60;
-    // swTimes[2] = msTime/100/60%60;
-    // swTimes[3] = msTime/100/60/60%24;
-    var unconverted = msTime;
-    for(var i = 0; i<swMaxs.length; i++){
-        swTimes[i] = unconverted%swMaxs[i];
-        unconverted = Math.floor(unconverted/swMaxs[i]);
+        this.swRun(curTime);
     }
 
-}
+    //used to stop sw
+    swStop(){
+        this.swRunning = false;
+    }
 
-//create display for sw given swTimes array
-function swFormatTime(){
-    var display = "";
-    for(var i = swTimes.length-1; i>0; i--){
-        var placeValue = swTimes[i];
-        if(placeValue < 10){
-            placeValue = "0"+placeValue;
+    swStartOrStop(){
+        if(this.swRunning){
+            this.swStop();
+            this.swStartStopBtn.textContent = "Start";
         }
-        display = display + placeValue + ":";
-    }
-
-    if(swTimes[0] < 10){
-        display = display + "00" + swTimes[0];
-    }
-    else if(swTimes[0]<100){
-        display = display + "0" +swTimes[0];
-    }
-    else{
-        display = display + swTimes[0];
-    }
-    return display;
-}
-
-//Lap Functions
-//Adds laps
-function swLap(){
-    var lap = document.createElement("li");
-    lap.textContent = swDisplayTxt;
-    lap.classList.add("swLap");
-    if(swLapsPanel.classList.contains("remove")){
-        swLapsPanel.classList.remove("remove");
-        swLapsPanel.classList.add("add");
-    }
-    if(swLaps.childElementCount >= lapPage * lapPerPage + lapPerPage){
-        lap.classList.add("remove");
-        if(swLapsDown.classList.contains("hide")){
-            swLapsDown.classList.remove("hide");
-            swLapsDown.classList.add("show");
-        }
-    }
-    swLaps.appendChild(lap);  
-}
-
-function swDownPg(){
-    var laps = swLaps.getElementsByClassName("swLap");
-    for(var i = 0; i < lapPerPage; i++){
-        laps[lapPage*lapPerPage+i].classList.remove("add");
-        laps[lapPage*lapPerPage+i].classList.add("remove");
-    }
-    lapPage++;
-    for(var i = 0; i < lapPerPage; i++){
-        if(lapPage*lapPerPage+i < laps.length){
-            laps[lapPage*lapPerPage+i].classList.remove("remove");
-            laps[lapPage*lapPerPage+i].classList.add("add");
+        else{
+            this.swStart();
+            this.swStartStopBtn.textContent = "Stop";
         }
     }
 
-    //add page up if not visible
-    if(swLapsUpBtn.classList.contains("hide")){
-        swLapsUpBtn.classList.remove("hide");
-        swLapsUpBtn.classList.add("show");
+    //used to reset sw
+    swReset(){
+        //reset stopwatch
+        this.swStop();
+        this.swStartStopBtn.textContent = "Start";
+        this.swAmount = 0;
+        this.swTimes = [0,0,0,0];
+        this.swDisplayTxt = "00:00:00:000";
+        this.swDisplay.textContent = this.swDisplayTxt;
+        //reset laps
+        this.swClearChildren(this.swLaps);
+        this.swLapsUpBtn.classList.remove("show");
+        this.swLapsUpBtn.classList.add("hide");
+        this.swLapsDownBtn.classList.remove("show");
+        this.swLapsDownBtn.classList.add("hide");
+        this.lapPage = 0;
+        this.swLapsPanel.classList.remove("add");
+        this.swLapsPanel.classList.add("remove");
     }
 
-    //remove pagedown if now on last page
-    if(lapPage * lapPerPage +lapPerPage >= swLaps.childElementCount){
-        swLapsDownBtn.classList.remove("show");
-        swLapsDownBtn.classList.add("hide");
-    }
-}
-
-function swUpPg(){
-    var laps = swLaps.getElementsByClassName("swLap");
-    for(var i = 0; i < lapPerPage; i++){
-        if(lapPage*lapPerPage+i < laps.length){
-            laps[lapPage*lapPerPage+i].classList.remove("add");
-            laps[lapPage*lapPerPage+i].classList.add("remove");
+    //sets and increments stopwatch value
+    swRun(prevTime){
+        if(this.swRunning){
+            var curTime = Date.now();
+            this.swAmount += curTime - prevTime;
+            this.swConvertFromMS(this.swAmount);
+            this.swDisplayTxt = this.swFormatTime();
+            this.swDisplay.textContent = this.swDisplayTxt;
+            this.swTimeout = setTimeout(() => this.swRun(curTime), 10);
+        }
+        else{
+            clearTimeout(this.swTimeout);
         }
     }
-    lapPage--;
-    for(var i = 0; i < lapPerPage; i++){
-        laps[lapPage*lapPerPage+i].classList.remove("remove");
-        laps[lapPage*lapPerPage+i].classList.add("add");
+
+    //conversion and formatting functions
+    //convert ms value into hr/min/sec/ms
+    swConvertFromMS(msTime){
+        // this.swTimes[0] = msTime%100;
+        // this.swTimes[1] = msTime/100%60;
+        // this.swTimes[2] = msTime/100/60%60;
+        // this.swTimes[3] = msTime/100/60/60%24;
+        var unconverted = msTime;
+        for(var i = 0; i<this.swMaxs.length; i++){
+            this.swTimes[i] = unconverted%this.swMaxs[i];
+            unconverted = Math.floor(unconverted/this.swMaxs[i]);
+        }
     }
 
-    //add page down if not visible
-    if(swLapsDownBtn.classList.contains("hide")){
-        swLapsDownBtn.classList.remove("hide");
-        swLapsDownBtn.classList.add("show");
+    //create display for sw given this.swTimes array
+    swFormatTime(){
+        var display = "";
+        for(var i = this.swTimes.length-1; i>0; i--){
+            var placeValue = this.swTimes[i];
+            if(placeValue < 10){
+                placeValue = "0"+placeValue;
+            }
+            display = display + placeValue + ":";
+        }
+
+        if(this.swTimes[0] < 10){
+            display = display + "00" + this.swTimes[0];
+        }
+        else if(this.swTimes[0]<100){
+            display = display + "0" +this.swTimes[0];
+        }
+        else{
+            display = display + this.swTimes[0];
+        }
+        return display;
     }
 
-    //remove pageup if now on last page
-    if(lapPage * lapPerPage === 0){
-        swLapsUpBtn.classList.remove("show");
-        swLapsUpBtn.classList.add("hide");
+    //Lap Functions
+    //Adds laps
+    swLap(){
+        var lap = document.createElement("li");
+        lap.textContent = this.swDisplayTxt;
+        lap.classList.add("swLap");
+        if(this.swLapsPanel.classList.contains("remove")){
+            this.swLapsPanel.classList.remove("remove");
+            this.swLapsPanel.classList.add("add");
+        }
+        if(this.swLaps.childElementCount >= this.lapPage * this.lapPerPage + this.lapPerPage){
+            lap.classList.add("remove");
+            if(this.swLapsDownBtn.classList.contains("hide")){
+                this.swLapsDownBtn.classList.remove("hide");
+                this.swLapsDownBtn.classList.add("show");
+            }
+        }
+        this.swLaps.appendChild(lap);  
+    }
+
+    swDownPg(){
+        var laps = this.swLaps.getElementsByClassName("swLap");
+        for(var i = 0; i < this.lapPerPage; i++){
+            laps[this.lapPage*this.lapPerPage+i].classList.remove("add");
+            laps[this.lapPage*this.lapPerPage+i].classList.add("remove");
+        }
+        this.lapPage++;
+        for(var i = 0; i < this.lapPerPage; i++){
+            if(this.lapPage*this.lapPerPage+i < laps.length){
+                laps[this.lapPage*this.lapPerPage+i].classList.remove("remove");
+                laps[this.lapPage*this.lapPerPage+i].classList.add("add");
+            }
+        }
+
+        //add page up if not visible
+        if(this.swLapsUpBtn.classList.contains("hide")){
+            this.swLapsUpBtn.classList.remove("hide");
+            this.swLapsUpBtn.classList.add("show");
+        }
+
+        //remove pagedown if now on last page
+        if(this.lapPage * this.lapPerPage +this.lapPerPage >= this.swLaps.childElementCount){
+            this.swLapsDownBtn.classList.remove("show");
+            this.swLapsDownBtn.classList.add("hide");
+        }
+    }
+
+    swUpPg(){
+        var laps = this.swLaps.getElementsByClassName("swLap");
+        for(var i = 0; i < this.lapPerPage; i++){
+            if(this.lapPage*this.lapPerPage+i < laps.length){
+                laps[this.lapPage*this.lapPerPage+i].classList.remove("add");
+                laps[this.lapPage*this.lapPerPage+i].classList.add("remove");
+            }
+        }
+        this.lapPage--;
+        for(var i = 0; i < this.lapPerPage; i++){
+            laps[this.lapPage*this.lapPerPage+i].classList.remove("remove");
+            laps[this.lapPage*this.lapPerPage+i].classList.add("add");
+        }
+
+        //add page down if not visible
+        if(this.swLapsDownBtn.classList.contains("hide")){
+            this.swLapsDownBtn.classList.remove("hide");
+            this.swLapsDownBtn.classList.add("show");
+        }
+
+        //remove pageup if now on last page
+        if(this.lapPage * this.lapPerPage === 0){
+            this.swLapsUpBtn.classList.remove("show");
+            this.swLapsUpBtn.classList.add("hide");
+        }
+    }
+
+    //Removes All Laps
+    swClearChildren(element){
+        while(element.lastChild){
+            element.removeChild(element.lastChild);
+        }
     }
 }
 
-//Removes All Laps
-function swClearChildren(element){
-    while(element.lastChild){
-        element.removeChild(element.lastChild);
-    }
-}
+let test = new Stopwatch(document.getElementsByClassName('stopwatch')[0]);
 
-swStartStopBtn.addEventListener("click", swStartOrStop);
-swResetBtn.addEventListener("click", swReset);
-swLapBtn.addEventListener("click", swLap);
-swLapsDownBtn.addEventListener("click", swDownPg);
-swLapsUpBtn.addEventListener("click", swUpPg);
+
+
